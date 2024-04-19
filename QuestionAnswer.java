85dd0c2e2f3dab5bbbb049019ce2d3522dc752a0
amishavermaaa <133965778+amishavermaaa@users.noreply.github.com>
2024-04-19 17:14:38 +0530

    Create QuestionAnswer.java

---
+++ QuestionAnswer.java
@@ -0,0 +1,26 @@
+package com.example.myquiz;
+
+public class QuestionAnswer {
+
+    public static String question[] = {
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Which of the following is an application software?",
+            "Which company owns the apple?"
+    };
+
+    public static String choices[][] = {
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"LINUX","macOS","UNIX","Graphics"},
+            {"Google","Apple","Nokia","Samsung"}
+
+    };
+
+    public static String correctAnswers[] = {
+            "Google",
+            "Notepad",
+            "Graphics",
+            "Apple"
+    };
+}
